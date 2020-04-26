c0879dc5541806f6cd1c4ddc607e603a755d2c6a
nikita312 <vazikovn1@gmail.com>
2020-04-26 13:25:18 +0300

    Update cachematrix.R
    solve for Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+      
+	inversed = NULL
+	
+	# Set the matrix
+	set = function(y) {                
+			x <<- y
+			inversed <<- NULL
+	}
+	
+	# Get the matrix
+	get = function() { x }
+	
+	# Set the inverse of the matrix
+	setinverse = function(inverse) { inversed <<- inverse }
+	
+	# Get the inverse of the matrix
+	getinverse = function() { inversed }
+	
+	# Return a list of the methods
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	# Return a matrix that is the inverse of 'x'        
+	inversed = x$getinverse()
+	
+	# Inverse has already been calculated
+	if (!is.null(inversed)){
+		# Get inverse matrix from cache
+		return(inversed)
+	}
+	
+	# Calculate the inverse of input matrix
+	matr = x$get()
+	inversed = solve(matr, ...)
+	
+	# Set the value of the inverse matrix in the cache
+	x$setinverse(inversed)        
+	return(inversed)
 }
